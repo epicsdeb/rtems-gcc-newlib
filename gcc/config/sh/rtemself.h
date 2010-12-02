69448a9b4112dbbc75e04d92964089ec114b06d8
Michael Davidsaver <mdavidsaver@bnl.gov>
2010-12-02 14:05:25 -0500

    RTEMS 4.10 GGC patch 20101003
    gcc-core-4.4.5-rtems4.10-20101003.diff

--- gcc/config/sh/rtemself.h
+++ gcc/config/sh/rtemself.h
@@ -24,3 +24,8 @@ along with GCC; see the file COPYING3.  If not see
   builtin_define( "__rtems__" );		\
   builtin_assert( "system=rtems" );		\
 } while (0)
+
+/* Use the defaults */
+#undef WCHAR_TYPE
+
+#undef WCHAR_TYPE_SIZE
